88ffbc5d6794c7b54f685fc332cef087ae1997e4
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-02-23 22:32:58 +0200

    Added tests to ensure that `AddCommentAsync` method throws expected exceptions.

--- CookingBlogBackend/PostApiService.Tests/IntegrationTests/Middlewares/ExceptionMiddlewareTests.cs
+++ CookingBlogBackend/PostApiService.Tests/IntegrationTests/Middlewares/ExceptionMiddlewareTests.cs
@@ -259,5 +259,60 @@ namespace PostApiService.Tests.IntegrationTests.Middlewares
 
             Assert.Equal(expectedMessage, errorResponse.Message);
         }
+
+        [Theory]
+        [InlineData(typeof(PostNotFoundException), "/api/comments/999", HttpStatusCode.NotFound)]
+        [InlineData(typeof(AddCommentFailedException), "/api/comments/1", HttpStatusCode.InternalServerError)]
+        [InlineData(typeof(DbUpdateException), "/api/comments/1", HttpStatusCode.InternalServerError)]
+        [InlineData(typeof(OperationCanceledException), "/api/comments/1", HttpStatusCode.RequestTimeout)]
+        [InlineData(typeof(TimeoutException), "/api/comments/1", HttpStatusCode.RequestTimeout)]
+        [InlineData(typeof(Exception), "/api/comments/1", HttpStatusCode.InternalServerError)]
+        public async Task AddCommentAsync_ShouldReturnExpectedStatusCode_WhenExceptionThrown
+            (Type exceptionType, string url, HttpStatusCode expectedStatus)
+        {
+            // Arrange            
+            var testPostId = 999;
+
+            Exception exception = exceptionType switch
+            {
+                Type t when t == typeof(PostNotFoundException) => new PostNotFoundException(testPostId),
+                Type t when t == typeof(AddCommentFailedException) => new AddCommentFailedException(testPostId),
+                Type t when t == typeof(DbUpdateException) => new DbUpdateException(ResponseErrorMessages.DbUpdateException),
+                Type t when t == typeof(OperationCanceledException) =>
+                new OperationCanceledException(ResponseErrorMessages.OperationCanceledException),
+                Type t when t == typeof(TimeoutException) => new TimeoutException(ResponseErrorMessages.TimeoutException),
+                Type t when t == typeof(Exception) => new Exception(ResponseErrorMessages.UnexpectedErrorException),
+                _ => throw new ArgumentException($"Unsupported exception type: {exceptionType}")
+            };
+
+            _factoryFixture.SetException(exception);
+
+            var comment = new Comment { Content = "Test comment" };
+
+            var content = HttpHelper.GetJsonHttpContent(comment);
+
+            // Act
+            var response = await _client.PostAsync(url, content);
+
+            // Assert
+            Assert.Equal(expectedStatus, response.StatusCode);
+
+            var errorResponse = await response.Content.ReadFromJsonAsync<ApiResponse<Comment>>();
+            Assert.NotNull(errorResponse);
+
+            string expectedMessage = exceptionType switch
+            {
+                Type t when t == typeof(PostNotFoundException) => string.Format
+                (PostErrorMessages.PostNotFound, testPostId),
+                Type t when t == typeof(AddCommentFailedException) => string.Format
+                (CommentErrorMessages.AddCommentFailed, testPostId),
+                Type t when t == typeof(DbUpdateException) => ResponseErrorMessages.DbUpdateException,
+                Type t when t == typeof(OperationCanceledException) => ResponseErrorMessages.OperationCanceledException,
+                Type t when t == typeof(TimeoutException) => ResponseErrorMessages.TimeoutException,
+                Type t when t == typeof(Exception) => ResponseErrorMessages.UnexpectedErrorException
+            };
+
+            Assert.Equal(expectedMessage, errorResponse.Message);
+        }
     }
 }
