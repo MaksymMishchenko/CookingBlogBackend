70dbd1453cacda5b044bb0eda617cc6a8481836d
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-04-04 15:59:59 +0300

    feat: add AnyAsync to IRepository and implement in Repository
    - Added AnyAsync method signature to IRepository
    - Implemented AnyAsync in Repository
    - Refactored AddAsync method for improved clarity and consistency

--- CookingBlogBackend/PostApiService/Repositories/IRepository.cs
+++ CookingBlogBackend/PostApiService/Repositories/IRepository.cs
@@ -1,12 +1,16 @@
-﻿namespace PostApiService.Repositories
+﻿using PostApiService.Models;
+using System.Linq.Expressions;
+
+namespace PostApiService.Repositories
 {
     public interface IRepository<T> where T : class, IEntity
     {
         Task<T?> GetByIdAsync(int id);
         Task<List<T>> GetAllAsync();
-        Task<T> AddAsync(T entity);
+        Task<T> AddAsync(T entity, CancellationToken cancellationToken);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
-        IQueryable<T> AsQueryable();        
+        IQueryable<T> AsQueryable();
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken);
     }
 }
--- CookingBlogBackend/PostApiService/Repositories/Repository.cs
+++ CookingBlogBackend/PostApiService/Repositories/Repository.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace PostApiService.Repositories
 {
     public class Repository<T> : IRepository<T> where T : class, IEntity
     {
-        private readonly DbContext _context;        
+        private readonly DbContext _context;
         private readonly DbSet<T> _dbSet;
 
         public Repository(DbContext context)
@@ -15,17 +16,17 @@ namespace PostApiService.Repositories
 
         public async Task<List<T>> GetAllAsync() => await _dbSet.ToListAsync();
 
-        public async Task<T> AddAsync(T entity)
+        public async Task<T> AddAsync(T entity, CancellationToken token = default)
         {
             await _dbSet.AddAsync(entity);
             await _context.SaveChangesAsync();
             return entity;
         }
 
-        public IQueryable<T> AsQueryable() => _dbSet.AsQueryable();
+        public IQueryable<T> AsQueryable() => _dbSet.AsQueryable().AsNoTracking();
 
         public async Task DeleteAsync(T entity)
-        {            
+        {
             _dbSet.Remove(entity);
             await _context.SaveChangesAsync();
         }
@@ -37,5 +38,14 @@ namespace PostApiService.Repositories
             _dbSet.Update(entity);
             await _context.SaveChangesAsync();
         }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate,
+                                   CancellationToken cancellationToken = default)
+        {
+            return await _dbSet
+                .AsQueryable()
+                .AsNoTracking()
+                .AnyAsync(predicate, cancellationToken);
+        }        
     }
 }
