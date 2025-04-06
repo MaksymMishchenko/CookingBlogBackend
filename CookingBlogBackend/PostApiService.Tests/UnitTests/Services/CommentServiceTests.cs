e20f4545ceed51fccef73bb406eab00f315a0ce5
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-04-06 20:24:42 +0300

    Refactor: Move mock dependencies to constructor

--- CookingBlogBackend/PostApiService.Tests/UnitTests/Services/CommentServiceTests.cs
+++ CookingBlogBackend/PostApiService.Tests/UnitTests/Services/CommentServiceTests.cs
@@ -10,29 +10,36 @@ namespace PostApiService.Tests.UnitTests.Services
 {
     public class CommentServiceTests
     {
+        private readonly IRepository<Comment> _mockCommentRepo;
+        private readonly IRepository<Post> _mockPostRepo;
+        private readonly IAuthService _mockAuthService;
+        public CommentServiceTests()
+        {
+            _mockCommentRepo = Substitute.For<IRepository<Comment>>();
+            _mockPostRepo = Substitute.For<IRepository<Post>>();
+            _mockAuthService = Substitute.For<IAuthService>();
+        }
+
         [Fact]
         public async Task AddCommentAsync_ShouldAddCommentToPost()
         {
             // Arrange            
             var postId = 1;
             var comment = new Comment { Content = "Test comment", Author = "Test author" };
-
-            var mockCommentRepo = Substitute.For<IRepository<Comment>>();
-            var mockPostRepo = Substitute.For<IRepository<Post>>();
-            mockPostRepo.AnyAsync(Arg.Any<Expression<Func<Post, bool>>>(), Arg.Any<CancellationToken>())
+                        
+            _mockPostRepo.AnyAsync(Arg.Any<Expression<Func<Post, bool>>>(), Arg.Any<CancellationToken>())
                 .Returns(true);
 
-            var testUser = new IdentityUser { Id = "user123" };
-            var mockAuthService = Substitute.For<IAuthService>();
-            mockAuthService.GetCurrentUserAsync().Returns(Task.FromResult(testUser));
+            var testUser = new IdentityUser { Id = "user123" };            
+            _mockAuthService.GetCurrentUserAsync().Returns(Task.FromResult(testUser));
 
-            var service = new CommentService(mockCommentRepo, mockPostRepo, mockAuthService);
+            var service = new CommentService(_mockCommentRepo, _mockPostRepo, _mockAuthService);
 
             // Act
             await service.AddCommentAsync(postId, comment);
 
             // Assert
-            await mockCommentRepo.Received(1)
+            await _mockCommentRepo.Received(1)
                 .AddAsync(comment, Arg.Any<CancellationToken>());
         }
 
@@ -43,22 +50,18 @@ namespace PostApiService.Tests.UnitTests.Services
             var commentId = 1;
             var existingPost = TestDataHelper.GetListWithComments()
                 .FirstOrDefault(c => c.Id == commentId);
-
-            var mockCommentRepo = Substitute.For<IRepository<Comment>>();
-            mockCommentRepo.GetByIdAsync(Arg.Any<int>()).Returns(existingPost);
-
-            var mockPostRepo = Substitute.For<IRepository<Post>>();
-            var mockAuthService = Substitute.For<IAuthService>();
+            
+            _mockCommentRepo.GetByIdAsync(Arg.Any<int>()).Returns(existingPost);            
 
             var updatedComment = new EditCommentModel { Content = "Content edited successfully" };
 
-            var service = new CommentService(mockCommentRepo, mockPostRepo, mockAuthService);
+            var service = new CommentService(_mockCommentRepo, _mockPostRepo, _mockAuthService);
 
             // Act
             await service.UpdateCommentAsync(commentId, updatedComment);
 
             // Assert
-            await mockCommentRepo.Received(1)
+            await _mockCommentRepo.Received(1)
                 .GetByIdAsync(Arg.Any<int>());
         }
 
@@ -70,20 +73,16 @@ namespace PostApiService.Tests.UnitTests.Services
 
             var post = TestDataHelper.GetListWithComments()
                 .FirstOrDefault(c => c.Id == commentId);
+            
+            _mockCommentRepo.GetByIdAsync(Arg.Any<int>()).Returns(post);            
 
-            var mockCommentRepo = Substitute.For<IRepository<Comment>>();
-            mockCommentRepo.GetByIdAsync(Arg.Any<int>()).Returns(post);
-
-            var mockPostRepo = Substitute.For<IRepository<Post>>();
-            var mockAuthService = Substitute.For<IAuthService>();
-
-            var service = new CommentService(mockCommentRepo, mockPostRepo, mockAuthService);
+            var service = new CommentService(_mockCommentRepo, _mockPostRepo, _mockAuthService);
 
             // Act
             await service.DeleteCommentAsync(commentId);
 
             // Assert            
-            await mockCommentRepo.Received(1)
+            await _mockCommentRepo.Received(1)
                 .GetByIdAsync(Arg.Any<int>());
         }
     }
