6b78d5d8caf631939d8c55d698f47ad3f49e9372
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-04-08 14:33:08 +0300

    Create IAuthRepository interface for user authentication operations

---
+++ CookingBlogBackend/PostApiService/Repositories/IAuthRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace PostApiService.Repositories
+{
+    public interface IAuthRepository
+    {
+        Task<IdentityUser> FindByNameAsync(string userName);
+        Task<IdentityUser> FindByEmailAsync(string email);
+        Task<IdentityUser> CreateAsync(IdentityUser user, string password);
+        Task<IdentityResult> AddClaimsAsync(IdentityUser user, Claim claim);
+        Task<IList<Claim>> GetClaimsAsync(IdentityUser user);
+        Task<IList<string>> GetRolesAsync(IdentityUser user);
+        Task<bool> CheckPasswordAsync(IdentityUser user, string password);
+        Task<IdentityUser> GetUserAsync(ClaimsPrincipal principal);
+    }
+}
