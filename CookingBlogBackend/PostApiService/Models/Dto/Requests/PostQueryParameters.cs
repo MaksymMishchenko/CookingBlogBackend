bf3e2890201ffeb8ab911c2e4580c1bf04fe197d
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-04-09 16:47:35 +0300

    feat: add PostQueryParameters DTO for cleaner query parameter handling in GetAllPostsAsync

---
+++ CookingBlogBackend/PostApiService/Models/Dto/Requests/PostQueryParameters.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PostApiService.Models.Dto.Requests
+{
+    public class PostQueryParameters
+    {
+        [FromQuery(Name = "pageNumber")]
+        public int PageNumber { get; set; } = 1;
+
+        [FromQuery(Name = "pageSize")]
+        public int PageSize { get; set; } = 10;
+
+        [FromQuery(Name = "commentPageNumber")]
+        public int CommentPageNumber { get; set; } = 1;
+
+        [FromQuery(Name = "commentsPerPage")]
+        public int CommentsPerPage { get; set; } = 10;
+
+        [FromQuery(Name = "includeComments")]
+        public bool IncludeComments { get; set; } = true;
+    }
+}
