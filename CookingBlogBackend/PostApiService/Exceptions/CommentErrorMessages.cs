97daa08ac252927b26210c37034da10f0eff49f6
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-04-14 21:13:03 +0300

    Remove unused CommentErrorMessages constants

--- CookingBlogBackend/PostApiService/Exceptions/CommentErrorMessages.cs
+++ CookingBlogBackend/PostApiService/Exceptions/CommentErrorMessages.cs
@@ -6,11 +6,6 @@
         public const string AddCommentFailed = "Failed to add the comment to the post with ID {0}.";
         public const string UpdateCommentFailed = "Failed to update the comment with ID {0}.";
         public const string DeleteCommentFailed = "Failed to delete the comment with ID {0}.";
-
-        public const string CommentCannotBeNull = "Comment cannot be null.";
-        public const string MismatchedPostId = "The postId in the request URL does not match the PostId in the comment.";        
         public const string InvalidCommentIdParameter = "Comment ID must be greater than 0.";
-        public const string ContentIsRequired = "Comment property 'Content' is required.";
-
     }
 }
