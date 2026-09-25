bffc03d0ba5aeedb1463e55934fc4b29ab055f21
MaksymMishchenko <mishchenkomv@hotmail.com>
2026-09-25 20:05:13 +0300

    chore(tests): remove obsolete unused helper code

--- CookingBlogBackend/PostApiService.Tests/Helper/HttpHelper.cs
+++ CookingBlogBackend/PostApiService.Tests/Helper/HttpHelper.cs
@@ -3,30 +3,9 @@
     public class HttpHelper
     {
         public static class Urls
-        {
-            public static class Categories
-            {                
-                public const string AdminBase = "/api/admin/categories";
-                public const string GetById = "/api/admin/categories/{0}";
-            }            
-
-            public static class Posts
-            {
-                public const string Base = "/api/publicposts";
-                public const string AdminBase = "/api/admin/posts";
-                public const string GetById = "/api/admin/posts/{0}";
-                public const string GetBySlug = "/api/publicposts/{0}/{1}";
-                public const string GetByCategorySlug = "/api/publicposts/category/{0}?pageNumber={1}&pageSize={2}";
-                public const string Search = "/api/publicposts/search?queryString={0}&pageNumber={1}&pageSize={2}";
-                public const string Paginated = "/api/publicposts?pageNumber={0}&pageSize={1}";
-                public const string AdminPaginated = "/api/admin/posts?search={0}&categorySlug={1}&onlyActive={2}&pageNumber={3}&pageSize={4}&sortBy={5}&sortDirection={6}";
-                public const string GetComments = "/api/publicposts/{0}/comments?pageNumber={1}&pageSize={2}";
-            }
-
+        {                                  
             public static class Comments
-            {
-                public const string Base = "/api/comments";
-
+            {                
                 public const string GetById = "/api/comments/{0}";
             }
 
